c5fb7f7b9f85ab1a9b4a0a6f8916f1678f4044bd
Harshitha55535 <Harshitharajana55535@gmail.com>
2024-03-19 11:56:47 +0530

    added Account page

--- src/Account.js
+++ src/Account.js
@@ -1,7 +1,25 @@
+import './Account.css';
 export default function Account(){
     return(
-        <div>
-            This is Account Page
-        </div>
-    )
+    <form action="#" method="post">
+      <h2>My Details</h2>
+      <label htmlFor="name">Name:</label>
+      <input type="text" id="name" name="name" defaultValue="John Doe" required />
+      
+      <label htmlFor="age">Age:</label>
+      <input type="number" id="age" name="age" defaultValue="30" required />
+      
+      <label htmlFor="mobile">Mobile:</label>
+      <input type="tel" id="mobile" name="mobile" defaultValue="123-456-7890" required />
+      
+      <label htmlFor="email">Email Address:</label>
+      <input type="email" id="email" name="email" defaultValue="john@example.com" required />
+      
+      <label htmlFor="password">Password:</label>
+      <input type="text" id="password" name="password" defaultValue="password123" required />
+      
+      <input type="submit" value="Save" />
+    </form>
+    );
 }
+
